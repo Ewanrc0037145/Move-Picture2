4983b6ff9149d206635a03eceb6ba04c8675e8af
Ewan Harris <rc0037145@student.riversidecollege.ac.uk>
2020-02-10 14:39:43 +0000

    fghghdrghcfh

--- Move Picture2/Form1.cs
+++ Move Picture2/Form1.cs
@@ -21,5 +21,21 @@ namespace Move_Picture2
         {
             pictureBox1.Location = new Point(pictureBox1.Location.X + 5, pictureBox1.Location.Y);
         }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Location = new Point(pictureBox1.Location.X - 5, pictureBox1.Location.Y);
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Location = new Point(pictureBox1.Location.X, pictureBox1.Location.Y + 5 );
+        }
+
+        private void Button4_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Location = new Point(pictureBox1.Location.X, pictureBox1.Location.Y - 5);
+        }
     }
 }
+
